8b057d920984dcf6f7b23ecf9831609343929d85
Marcos Tullio <marcostullio.s@gmail.com>
2024-03-18 23:47:51 -0300

    implementando a lógica

--- src/script.js
+++ src/script.js
@@ -12,8 +12,15 @@ function criptoValue() {
         const regex = new RegExp(vogal, 'g')
         inputValue = inputValue.replace(regex, dict[vogal]);
     }
-    document.getElementById("criptoValue").innerText = inputValue;
+    // Oculta a imagem e o texto original
+    document.getElementById("result-image").style.display = "none";
+    document.getElementById("result-text").style.display = "none";
+    // Exibe o resultado da criptografia
+    document.getElementById("result-text").style.display = "block";
+    document.getElementById("result-text").getElementsByTagName("h1")[0].style.display = "none";
+    document.getElementById("result-text").getElementsByTagName("p")[0].innerText = inputValue;
 }
+
 function descriptoValue() {
     var inputValue = document.getElementById("inputValue").value;
     const dict = {
@@ -28,5 +35,11 @@ function descriptoValue() {
         const regex = new RegExp(vogal, 'g')
         inputValue = inputValue.replace(regex, dict[vogal]);
     }
-    document.getElementById("criptoValue").innerText = inputValue;
+    // Oculta a imagem e o texto original
+    document.getElementById("result-image").style.display = "none";
+    document.getElementById("result-text").style.display = "none";
+    // Exibe o resultado da descriptografia
+    document.getElementById("result-text").style.display = "block";
+    document.getElementById("result-text").getElementsByTagName("h1")[0].style.display = "none";
+    document.getElementById("result-text").getElementsByTagName("p")[0].innerText = inputValue;
 }
